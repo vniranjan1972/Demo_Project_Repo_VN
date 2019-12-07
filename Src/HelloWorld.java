4a7e8e3bbdc26272d0845201d4b9fa440215ae14
vniranjan1972 <vniranjan72@outlook.com>
2019-12-07 19:12:49 +0530

    Update HelloWorld.java

--- Src/HelloWorld.java
+++ Src/HelloWorld.java
@@ -4,6 +4,6 @@ class HelloWorld
 { 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("Hello, World - Bangalore, KA, India"); 
+		System.out.println("Hello, World - Bangalore, KAR, India"); 
 	} 
 } 
